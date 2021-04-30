bcf1de7ee8bc659f10a1dc6b5bef79b427b35141
AUTOPILOTyoutube <q3q2q1q@yandex.ru>
2021-04-30 15:06:49 +0300

    Модернизировал функцию создания скриншотов

--- EXAMPLES/ScreenShot.js
+++ EXAMPLES/ScreenShot.js
@@ -2,9 +2,25 @@
 //ДЕЛАЕТ СКРИНШОТ ЭКРАНА И СОХРАНЯЕТ ЕГО В ФАЙЛ screen.png
 
 auto(); //Проверка, что включены все необходимые разрешения для auto.js
-requestScreenCapture(true); //Разрешение на получение снимков экрана
 
-//ДЕЛАЕМ СКРИНШОТ ЭКРАНА И СОХРАНЯЕМ ЕГО В ФАЙЛ screen.png
-images.save(captureScreen(),'screen.png', 'png', 100);
+var options = ["Вертикальный", "Горизонтальный"]
+var i = dialogs.select("Выберите ориентацию экрана", options);
 
-alert('Скриншот был успешно сделан!')
+sleep(1000); //Пауза
+
+if(i >= 0){
+    if (options[i]=="Вертикальный"){
+        requestScreenCapture(true); //Разрешение на получение снимков вертикального экрана
+
+        images.save(captureScreen(),'screen.png', 'png', 100);
+        alert('Был успешно сделан скриншот screen.png')
+    }
+    if (options[i]=="LIMITED SERIES and WORLD SERIES"){
+        requestScreenCapture(false); //Разрешение на получение снимков горизонтального экрана
+
+        images.save(captureScreen(),'screen.png', 'png', 100);
+        alert('Был успешно сделан скриншот screen.png')
+    }
+}else{
+    toast("Вы ничего не выбрали! Программа завершена!");
+}
